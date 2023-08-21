a8f1d0bf885fcbb14666b59f377461b1001a9f38
Bhavya Sachdev <bhavyasachdev78@gmail.com>
2023-08-21 14:18:56 +0530

    Pushing Code Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,component:LandingPageComponent},
+  {path:'home' ,component:HomeComponent},
+  {path:'aboutMe' ,component:AboutComponent},
+  {path:'contactMe' ,component:ContactMeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharedModules/navbar/navbar.component';
+import { FooterComponent } from './sharedModules/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactMeComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
