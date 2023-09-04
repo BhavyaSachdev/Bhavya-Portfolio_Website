344ede151a2ca1c0c60e96a16c75d7f761da3a93
Bhavya Sachdev <505054148@penske.com>
2023-09-04 19:36:47 +0530

    Minor Enhancements

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  public nameStr : string = '';
+
   constructor() { }
 
   ngOnInit(): void {
-    
+    // setInterval( () => this.autoTying('Bhavya Sachdev.'),5000);
+    this.autoTying('Bhavya Sachdev.✨')
   }
+    
+autoTying(yourName:any){
+  Array.from(yourName).forEach((element, i ) => {
+    setTimeout(() => {
+      this.nameStr += element;
+    }, i * 170);
+  });
+  // this.nameStr =''
+  }
+
 }
